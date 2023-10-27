ca92534d3d439947251530eba4b72320f519cf31
Zanchenling <wangzancl@gmail.com>
2023-10-27 16:34:10 -0400

    modified:   notebooks/hb_fft.ipynb     Added:         resampling method         multi-watershed analysis         pH plot         missing data ratio
    new file:   notebooks/nmf.ipynb
    modified:   src/fft_utils.py
        refactored
    modified:   src/hb_dic.py
        refactored

--- src/fft_utils.py
+++ src/fft_utils.py
@@ -1,14 +1,37 @@
 import numpy as np
-from numpy.typing import ArrayLike
+import numpy.typing as npt
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
-def spectrum(x: ArrayLike):
+def spectrum(x: npt.ArrayLike):
     fourier = np.fft.rfft(x)[1:]  # [1:] means neglecting 0 Hz signal (baseline)
     power_specturm = np.abs(fourier) ** 2
     power_specturm /= np.sum(power_specturm)  # normalized power
-    f = np.fft.rfftfreq(len(x))[1:]  # frequencies of FFT results
-    Ts = (
-        1 / f
-    )  # Periods as indices of power specturm. The unit is the interval of original series
-    return pd.Series(power_specturm, index=Ts)
+    f = np.fft.rfftfreq(len(x))[1:]  # frequencies of real FFT results
+    # np.fft.rfftfreq(len(x)) yields an equally distributed array of frequencies from 0 to 0.5
+    # 0 means the baseline/background of the signal
+    # 0.5 implies a signal with a period of 1 / 0.5=2 in the original series
+    t = 1 / f  # Periods as indices of power specturm. The unit is the interval of original series
+    return pd.Series(power_specturm, index=t, name="Normalized Power")
+
+class FourierSpectrumPlot():
+    def __init__(self, power_spectrum:pd.Series, ax=None) -> None:
+        self.power_spectrum = power_spectrum
+        if ax is None:
+            ax = plt.gca()
+        self.ax = ax
+        return
+    
+    def plot(self):
+        self.ax.stem(self.power_spectrum.index, self.power_spectrum, markerfmt="", basefmt="C0-")
+        self.ax.set_xscale("log")
+        self.ax.set_xlabel("Period (Week)")
+        self.ax.set_ylabel("Normalized Power")
+        return
+
+
+
+
+
+        
--- src/hb_dic.py
+++ src/hb_dic.py
@@ -1,17 +1,62 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
-def load_dic(watershed: str):
-    return pd.read_excel(
+def load_watershed_data(
+    watershed: str | list, col: str | list[str]
+) -> pd.Series | pd.DataFrame:
+    usecols = ["date"]
+    if isinstance(col, str):
+        usecols.append(col)
+    elif isinstance(col, list):
+        usecols.extend(col)
+
+    df = pd.read_excel(
         "../data/HB_weekly_stream_chem/watersheds.xlsx",
         sheet_name=watershed,
         index_col="date",
-        usecols=["date", "DIC"],
+        usecols=usecols,
     )
+    if isinstance(watershed, str) and isinstance(col, str):
+        df = df.squeeze()
+    
+    return df
 
 
-def clean_dic_series(dic_series: pd.Series):
-    ds = dic_series.squeeze()
-    ds = ds[ds.index.drop_duplicates()]
-    ds.interpolate(inplace=True)
-    return ds
+def load_DIC_series(watershed: str):
+    return load_watershed_data(watershed, "DIC")
+
+
+def load_pH_series(watershed: str):
+    return load_watershed_data(watershed, "pH")
+
+
+class SeriesPlotWithMissingValues:
+    def __init__(self, data: pd.Series, ax=None):
+        self.data = data
+
+        missing = data[data.isna()]
+        self.missing = missing
+
+        if ax is None:
+            ax = plt.gca()
+        self.ax = ax
+        return
+
+    def plot(self, missing_yloc=None):
+        self.data.dropna().plot(ax=self.ax)
+
+        ymin, ymax = self.ax.get_ylim()
+        if missing_yloc is None:
+            missing_yloc = 0.98*ymin+0.02*ymax
+        missing = self.missing.copy()
+        missing[:] = missing_yloc
+        
+        missing.plot(
+            ax=self.ax,
+            linestyle="",
+            marker="|",
+            markersize=1,
+            color="r",
+        )
+        return
